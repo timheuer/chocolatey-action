ce8d0dfdb1ddc777d14b746fac8d8107a354deae
Tim Heuer <tim@timheuer.com>
2020-01-22 10:02:25 -0800

    Initial

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,11 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as exec from '@actions/exec'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    await exec.exec(
+      `choco install vswhere -v --cache-location=vswhere`
+    )
   } catch (error) {
     core.setFailed(error.message)
   }
